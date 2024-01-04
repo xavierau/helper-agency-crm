40058961f2f830506d56dbea0d8fd42e03d0b32d
Xavier Au <xavier.au@gmail.com>
2024-01-04 19:04:03 +0800

    update

--- config/database.php
+++ config/database.php
@@ -79,18 +79,23 @@ return [
         ],
 
         'activity_logs' => [
-            'driver' => env('ACTIVITY_LOG_DB_CONNECTION', 'pgsql'),
-            'url' => env('ACTIVITY_LOG_DATABASE_URL'),
-            'host' => env('ACTIVITY_LOG_DB_HOST', '127.0.0.1'),
-            'port' => env('ACTIVITY_LOG_DB_PORT', '5432'),
-            'database' => env('ACTIVITY_LOG_DB_DATABASE', 'forge'),
-            'username' => env('ACTIVITY_LOG_DB_USERNAME', 'postgres'),
-            'password' => env('ACTIVITY_LOG_DB_PASSWORD', 'password'),
-            'charset' => 'utf8',
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            'schema' => 'public',
-            'sslmode' => 'prefer',
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
         'sqlsrv' => [
